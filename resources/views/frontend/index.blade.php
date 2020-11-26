97cc1ace1f1d2e0842c709bf519d414b78cb1fa0
Farhad-Hossain <farhad.cse33@gmail.com>
2020-11-26 20:05:02 +0600

    frontend: coupon

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -156,7 +156,7 @@
                         <div class="top">
                             <a href="{!! route('frontend.food.details', $food->id) !!}"><div class="bg-gradient"></div></a>
                             <div class="top-img">
-                                <img src="{!! asset('uploads') !!}/{!! $food->image1 !!}" alt="" style="width: 100%; max-height: 185px;">
+                                <img src="{!! asset('uploads') !!}/{!! $food->image1 !!}" alt="" style="width: 100%; min-height: 185px;">
                             </div>
                             <div class="logo-img">
                                 <img src="{!! asset('uploads') !!}/{!! $food->restaurant->logo !!}" alt="" style="width: 70px; height: 70px;">
